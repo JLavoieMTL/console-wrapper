4c375081f641b2f2f617362928bd6832827db43d
Joel Lavoie <jl@2klic.com>
2017-11-01 21:33:22 -0400

    First commit

---
+++ index.ts
@@ -0,0 +1,90 @@
+/*
+* Author: Joel Lavoie <joel.lavoie.ing@gmail.com>
+* Date: November 2017
+* License: ISC
+* Description: this class is a wrapper around console that allow to show the proper level of logs
+*              in the code without touching the console.x occurrences in the code and without having
+*              inject a logger in all constructors that need some logging. Instantiate this code once
+*              will overwrite all consoles.
+*/
+
+// Log levels
+const TRACE = 0
+const DEBUG = 1
+const INFO = 2
+const WARN = 3
+const ERROR = 4
+
+const LEVELS = {
+    trace: TRACE,
+    debug: DEBUG,
+    info: INFO,
+    warn: WARN,
+    error: ERROR
+}
+
+class Logger {
+
+    constructor(opt) {
+        var level = LEVELS[ opt.level ? opt.level : TRACE ]
+
+        console.trace = function (...args) {
+            if (level <= TRACE) {
+                if(opt && opt.trace && opt.trace.callback) opt.trace.callback(...args)
+                return Function.prototype.bind.call(console.trace, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.debug = function (...args) {
+            if (level <= DEBUG) {
+                if(opt && opt.debug && opt.debug.callback) opt.debug.callback(...args)
+                return Function.prototype.bind.call(console.debug, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.dir = function (...args) {
+            if (level <= DEBUG) {
+                if(opt && opt.dir && opt.dir.callback) opt.dir.callback(...args)
+                return Function.prototype.bind.call(console.dir, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.info = function (...args) {
+            if (level <= INFO) {
+                if(opt && opt.info && opt.info.callback) opt.info.callback(...args)
+                return Function.prototype.bind.call(console.info, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.warn = function (...args) {
+            if (level <= WARN) {
+                if(opt && opt.warn && opt.warn.callback) opt.warn.callback(...args)
+                return Function.prototype.bind.call(console.warn, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.error = function (...args) {
+            if (level <= ERROR) {
+                if(opt && opt.error && opt.error.callback) opt.error.callback(...args)
+                return Function.prototype.bind.call(console.error, console, ...args)
+            }
+            else return () => { }
+        }()
+
+        console.log = function (...args) {
+            if (level <= ERROR) {
+                if(opt && opt.log && opt.log.callback) opt.log.callback(...args)
+                return Function.prototype.bind.call(console.log, console, ...args)
+            }
+            else return () => { }
+        }()
+
+    }
+}
+
+export { Logger }
---
+++ readme.md
@@ -0,0 +1,65 @@
+# Requirements
+This library will work with any typescript project such as:
+
+ - nodeJS + typescript
+ - Angular2 + typescript
+
+# Description
+I saw in many projects developers who use console.log, console.warn, etc.. everywhere in their code. Then, they want to push their code to production servers. But oh!! The logs! They are spreaded everywhere in the code and they are useful. Using a new logger library to replace all these logs can be painful and somewhat cumbersome. So why not just wrapping all console by a logger, that would let the code untouched ? Well, this is exactly what this library does! :)
+
+This library is a wrapper around console that allow to show the proper level of logs in the code without touching the console.x occurrences in your code and without having to write a logger and inject it in all functions that need some logging. Instantiate this code once will allow you to set the log level without touching your console.X methods in your code.
+
+# Log level
+The following rules is applied:
+
+**TRACE < DEBUG < INFO < WARN < ERROR**
+
+So setting the log level to "trace" will show all the detailed logs. But obviously, in production, you probably prefer to only show a "warn" or "error" level.
+
+In a more graphic way, here is how the selection rule works. In the following table, the vertical header shows the level of the logging request, designated by p, while the horizontal header shows effective level of the logger, designated by q. The intersection of the rows (level request) and columns (effective level) is the boolean resulting from the basic selection rule.
+	![Levels](https://i.stack.imgur.com/Z5mag.png)
+
+# How to use
+
+At the beginning of your program, call the following:
+
+    import { Logger } from 'console-logger'
+    new Logger({ level: 'trace' })
+
+# Available console methods
+The following methods are wrapped:
+
+ - trace
+ - debug
+ - info
+ - warn
+ - error
+ - dir
+ - log
+
+# Examples
+
+## Basic logger with log filtering
+Suppose you're in prod, you may want to disable all logs:
+
+
+    import { Logger } from 'console-logger'
+    new Logger({ level: 'off' })
+
+## Run additional logic when console methods are called
+
+    import { Logger } from 'console-logger'
+    const options = {
+       level: 'off',
+       info: {
+	       callback: function(){
+		       // so something here
+		   }
+	   },
+	   error: {
+	       callback: function(){
+		       reportToCloud('blah', arguments); // send error to remote server
+		   }
+	   }
+    }
+    new Logger(options)
